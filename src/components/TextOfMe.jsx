6435af57f18bd511dc540652dbf542b8de467379
criscasguitar <criscascor9@gmail,com>
2023-05-27 12:07:43 -0600

    change url

--- src/components/TextOfMe.jsx
+++ src/components/TextOfMe.jsx
@@ -10,7 +10,7 @@ function TextOfMe() {
             <h1>Hello, my name is Cristian Castro, FullStack Developer</h1>
             <p>I am <span>Cristian Castro</span>, apassionate for coding, i love keep learning about more tecnologies, lenguages, and frameworks, tools that's make the process simple. I love to see the process when an application is built, besides being part of it. </p>
       
-             <a className='link-btn' href="https://drive.google.com/file/d/1XgHr9H0IhaSG4MI2cYj2CW0LYP-TjA9m/view?usp=sharing" target="_blank" rel='noopener noreferrer'>View Resume</a>
+             <a className='link-btn' href="https://drive.google.com/file/d/1efov9u20cYj1kHVCCFZ-VpSv4kF5HQS3/view?usp=sharing" target="_blank" rel='noopener noreferrer'>View Resume</a>
        
         </div>
 

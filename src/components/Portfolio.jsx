813a82677f0a85b151cdef02bbff44cc94143e62
criscasguitar <criscascor9@gmail,com>
2023-07-05 20:22:38 -0600

    added media queries

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -24,55 +24,70 @@ function Portfolio() {
           <div className='description' data-aos="fade-up">
             <h2 className='title'>Stage Spent</h2>
             <img src={image1} alt="Web Site image" />
-                <p>This project was created with React JS, using localStorage, usesState, styled components. The project serve like a spent planner, you can add more money, filter expenses by category, add, delet and edit (CRUD) expenses.</p>
-            <button className='portfolioButton'>
-                <a href="https://exquisite-rolypoly-3d139e.netlify.app/" target='_blank'>Go to Web Site</a>
-            </button>
+                <p>This project was created with React JS. The project serve like a spent planner, you can add more money, filter expenses by category, add, delet and edit (CRUD) expenses.</p> 
+                <a href="https://exquisite-rolypoly-3d139e.netlify.app/" target='_blank'>
+                  <button className='portfolioButton'>
+                      Go to Web Site
+                  </button>
+                </a>
+           
           </div>
 
           <div className='description' data-aos="fade-down">
             <h2 className='title'>FollowUp Veterinary</h2>
             <img src={imageVet} alt="Web Site image" />
                 <p>This project was created with React.JS, Tailwind CSS, Hooks, useState, useEffect, localStorage; it has a CRUD of patients.</p>
-            <button className='portfolioButton'>
-                <a href="https://bespoke-dodol-dd8966.netlify.app/" target='_blank'>Go to Web Site</a>
-            </button>
+                <a href="https://bespoke-dodol-dd8966.netlify.app/" target='_blank'>
+                  <button className='portfolioButton'>
+                      Go to Web Site
+                  </button>
+                </a>
           </div>
 
           <div className='description' data-aos="fade-up">
             <h2 className='title'>Rock & EDM Festival</h2>
             <img src={imageFest} alt="Web Site image" />
                 <p>Rock & EDM Festival is an informative website, wich simulates a music festival website. This project was builded with PHP, JavaScript, HTML, CSS, SASS.</p>
-            <button className='portfolioButton'>
-                <a href="https://singular-florentine-bb58fb.netlify.app/" target='_blank'>Go to Web Site</a>
-            </button>
+
+                <a href="https://singular-florentine-bb58fb.netlify.app/" target='_blank'>
+                  <button className='portfolioButton'>
+                      Go to Web Site
+                  </button>
+                </a>
+            
           </div>
 
           <div className='description' data-aos="fade-down">
             <h2 className='title'>UpTask</h2>
             <img src={imageUpTask} alt="Web Site image" />
                 <p>UpTask is a task planner, was created with PHP, JavaScript, SASS, npm, SQL, MySQL, HTML, with design pattern MVC, the API was created by me, the conection into databases, and response with json.</p>
-            <button className='portfolioButton'>
-                <a href="https://uptaskmvc.alwaysdata.net/" target='_blank'>Go to Web Site</a>
-            </button>
+            <a href="https://uptaskmvc.alwaysdata.net/" target='_blank'>
+                  <button className='portfolioButton'>
+                      Go to Web Site
+                  </button>
+                </a>
           </div>
 
           <div className='description' data-aos="fade-up">
             <h2 className='title'>GuitarLA</h2>
             <img src={imageGuitarLA} alt="Web Site image" />
-                <p>This project was created with React.Js, Strapi, APIS, Cloudinary, and render, learning differents ways to connect APIS, and more than one API; GuitarLA is a project that simulates a Guitar Stores.</p>
-            <button className='portfolioButton'>
-                <a href="https://guitarla-next-lac.vercel.app/" target='_blank'>Go to Web Site</a>
-            </button>
+                <p>This project was created with React.Js, Strapi, APIS, Cloudinary, and render, learning differents ways to connect APIS, and more than one API.</p>
+            <a href="https://guitarla-next-lac.vercel.app/" target='_blank'>
+                  <button className='portfolioButton'>
+                      Go to Web Site
+                  </button>
+                </a>
           </div>
 
           <div className='description' data-aos="fade-up">
             <h2 className='title'>Sales Meals</h2>
             <img src={salesMeals} alt="Web Site image" />
                 <p>This project was created with React.Js, Prisma, Next.js, MySql, and TailwindCSS, it simulates a web app of Sales of fast or junk food, there you could send, edit and delete your orders.</p>
-            <button className='portfolioButton'>
-                <a href="https://salemeals-app-production.up.railway.app/" target='_blank'>Go to Web Site</a>
-            </button>
+            <a href="https://salemeals-app-production.up.railway.app/" target='_blank'>
+                  <button className='portfolioButton'>
+                      Go to Web Site
+                  </button>
+                </a>
           </div>
         </div>
 
